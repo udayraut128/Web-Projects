8df2d109dde861cb96a8cefba12f92fdece6bf4e
Uday Raut <udayraut128@gmail.com>
2024-02-20 17:34:15 +0530

    change in js

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -2,7 +2,8 @@
 const birthdateInput = document.getElementById('birthdate');
 const calculateButton = document.getElementById('calculate-btn');
 const resultElement = document.getElementById('result');
-
+const paragraph = document.getElementById('paragraph');
+const paragraph1 = document.getElementById('paragraph1');
 // Add click event listener to the calculate age button
 calculateButton.addEventListener('click', calculateAge);
 
@@ -10,7 +11,7 @@ calculateButton.addEventListener('click', calculateAge);
 function calculateAge() {
   const birthdate = new Date(birthdateInput.value);
   const today = new Date();
-  
+
   // Calculate the age
   const ageInMilliseconds = today - birthdate;
   const ageInYears = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
@@ -18,7 +19,7 @@ function calculateAge() {
   // Display the result
   if(ageInYears >= 0) {
       resultElement.textContent = `Now, Your are ${ageInYears} years old!`;
-  }else{
-    resultElement.textContent = 'Not valid input';
-  }
+    }else{
+        resultElement.textContent = 'Not valid input';
+    }
 }
